e0e659446d47c0d788dbf59c36a60a0d87781854
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-06-17 00:49:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function insert_Row() {
     //Write your code here
   let sampleTable = document.getElementById("sampleTable");
    sampleTable.innerHTML = `
-	  <tr><td>New cell1</td> 
-	  <td>New cell2</td></tr> 
+	  <tr><td>New Cell1</td> 
+	  <td>New Cell2</td></tr> 
   `+sampleTable.innerHTML;	
   
 }
